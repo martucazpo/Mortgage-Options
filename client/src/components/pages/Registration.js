b25c66200cbfb710d030475848a15a00ef4c303d
Diego Barillas <diegobarillas@Diegos-MacBook-Pro.local>
2019-10-02 20:31:10 -0700

    tax

--- client/src/components/pages/Registration.js
+++ client/src/components/pages/Registration.js
@@ -13,7 +13,8 @@ class Registration extends Component {
     // name: "",
     // email: "",
     desiredPayment: "",
-    downPayment: ""
+    downPayment: "",
+    taxBracket: ""
   };
 
   componentDidMount() {
