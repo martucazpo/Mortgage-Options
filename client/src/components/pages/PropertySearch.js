36d378717cfa499102122c546a04d2ad0510a866
Matt Wright <mwright8929@gmail.com>
2019-10-09 20:46:00 -0700

    added basic styling to all of the pages and cleaned up the property page

--- client/src/components/pages/PropertySearch.js
+++ client/src/components/pages/PropertySearch.js
@@ -1,80 +1,74 @@
 import React, { Component } from "react";
 import API from "../../utils/API";
 import LinkList from "../linksList";
-import {withRouter} from 'react-router-dom';
+import { withRouter } from "react-router-dom";
 import Navbar from "../layout/Navbar";
-import { Link } from 'react-router-dom';
+import { Link } from "react-router-dom";
+import "./PropertySearch.css";
 
 let imageArr = [];
 let dataArr = [];
 
-
 class PropertySearch extends Component {
-  constructor (props) {
+  constructor(props) {
     super(props);
     this.state = {
-        result: {},
-        search: "",
-        ListPrice: 0,
-        TaxAnnualAmount: 0,
-        img: "",
-        listings: [],
-        email:"",
-        profileId : "",
-        savedProp : [],
-        _id : ""
+      result: {},
+      search: "",
+      ListPrice: 0,
+      TaxAnnualAmount: 0,
+      img: "",
+      listings: [],
+      email: "",
+      profileId: "",
+      savedProp: [],
+      _id: ""
     };
     this.renderSavedProps = this.renderSavedProps.bind(this);
     this.searchProperties = this.searchProperties.bind(this);
     this.renderListings = this.renderListings.bind(this);
     this.renderImages = this.renderImages.bind(this);
     this.handleLocationReload = this.handleLocationReload.bind(this);
-    
-  } 
-
+  }
 
   componentDidMount() {
-    API.getUser({email:this.props.match.params.email})
-    .then(res => {
-      console.log(res); 
+    API.getUser({ email: this.props.match.params.email }).then(res => {
+      console.log(res);
       this.setState({
-        name : res.data.name,
-        email : res.data.email,
-        profileId : res.data.profile[0]
-      })
+        name: res.data.name,
+        email: res.data.email,
+        profileId: res.data.profile[0]
+      });
     });
 
     API.getProfile(this.state.profileId)
-    .then(res => {
-      this.setState({
-        downPayment : res.data[0].downPayment,
-        desiredPayment : res.data[0].desiredPayment,
-        loanTerm : res.data[0].loanTerm,
-        propertyId : res.data[0].property,
-        profileId : res.data[0]._id
-        }, () => {
-
-          API.findPropertyAndPop(this.state.profileId)
-          .then(res =>
-            this.setState({
-              savedProp: res.data.property,
-              // name: this.state.name,
-              // email: this.state.email,
-              ListPrice: res.data.property.ListPrice,
-              TaxAnnualAmount: res.data.property.TaxAnnualAmount,
-              _id: res.data.property._id,
-              img: res.data.property.img
-            })
-            )
+      .then(res => {
+        this.setState(
+          {
+            downPayment: res.data[0].downPayment,
+            desiredPayment: res.data[0].desiredPayment,
+            loanTerm: res.data[0].loanTerm,
+            propertyId: res.data[0].property,
+            profileId: res.data[0]._id
+          },
+          () => {
+            API.findPropertyAndPop(this.state.profileId).then(res =>
+              this.setState({
+                savedProp: res.data.property,
+                // name: this.state.name,
+                // email: this.state.email,
+                ListPrice: res.data.property.ListPrice,
+                TaxAnnualAmount: res.data.property.TaxAnnualAmount,
+                _id: res.data.property._id,
+                img: res.data.property.img
+              })
+            );
+          }
+        );
       })
-    })
-    .catch(err => console.log(err));
+      .catch(err => console.log(err));
     this.searchProperties();
   }
-  
-
-    
-
 
   searchProperties() {
     API.search()
@@ -92,64 +86,166 @@ class PropertySearch extends Component {
             img: imageArr[i].Media
           });
         }
-        this.setState({ listings: dataArr
-         });
+        this.setState({ listings: dataArr });
       })
-      .then(console.log(dataArr,imageArr))
+      .then(console.log(dataArr, imageArr))
       .catch(err => console.log(err));
-    
   }
-      
+
   renderImages = img => {
     if (img <= 0) {
-      return;
+      return (
+        <img
+          value={"../../images/noImage.png"}
+          key={"../../images/noImage.png"}
+          src={"../../images/noImage.png"}
+          alt={"../../images/noImage.png"}
+        />
+      );
     }
     const oneImage = [img[0]];
     return oneImage.map(image => (
-      <img 
+      <img
         value={image.MediaURL}
         key={image.MediaUrl}
         src={image.MediaURL}
         alt={""}
-        style={{ height: "100px", width: "100px" }}
       />
     ));
   };
 
-
   handleForm = (ListPrice, TaxAnnualAmount, img) => {
-    
     API.saveProperty({
       ListPrice,
       TaxAnnualAmount,
       img: img.MediaURL,
-      id:this.state.profileId
+      id: this.state.profileId
     })
       .then(console.log("property saved!"))
-        
+
       .catch(err => console.log(err));
-      this.handleLocationReload();
+    this.handleLocationReload();
   };
 
   renderListings = () => {
     const listHtml = this.state.listings.map(list => (
       <div>
-        <strong>List Price {list.ListPrice}</strong>
-        <p>Annual Tax Amount {list.TaxAnnualAmount}</p>
-        {this.renderImages(list.img)}
-        <button
-          onClick={() => this.handleForm(
-            list.ListPrice,
-            list.TaxAnnualAmount,
-            list.img.length > 0 ? list.img[0] : [],
-          )}
-          className="btn btn-primary"
-          style={{ marginTop: "5px" }}
-        >
-          Save Property
-        </button>
+        {/* <div className="row"> */}
+        <div className="col s3">
+          <div className="card">
+            <div className="card-image">
+              {this.renderImages(list.img)}
+              <span className="card-title">Card Title</span>
+              <a
+                className="btn-floating halfway-fab waves-effect waves-light red"
+                onClick={() =>
+                  this.handleForm(
+                    list.ListPrice,
+                    list.TaxAnnualAmount,
+                    list.img.length > 0 ? list.img[0] : []
+                  )
+                }
+              >
+                <i class="material-icons">add</i>
+              </a>
+            </div>
+            <div className="card-content">
+              <p>List Price {list.ListPrice}</p>
+              <p>Annual Tax Amount {list.TaxAnnualAmount}</p>
+            </div>
+          </div>
+        </div>
+
+        {/* 
+          <div className="col s3">
+            <div className="card">
+              <div className="card-image">
+                {this.renderImages(list.img)}
+                <span className="card-title">Card Title</span>
+                <a
+                  className="btn-floating halfway-fab waves-effect waves-light red"
+                  onClick={() =>
+                    this.handleForm(
+                      list.ListPrice,
+                      list.TaxAnnualAmount,
+                      list.img.length > 0 ? list.img[0] : []
+                    )
+                  }
+                >
+                  <i class="material-icons">add</i>
+                </a>
+              </div>
+              <div className="card-content">
+                <p>List Price {list.ListPrice}</p>
+                <p>Annual Tax Amount {list.TaxAnnualAmount}</p>
+              </div>
+            </div>
+          </div>
+
+
+
+
+
+          <div className="col s3">
+            <div className="card">
+              <div className="card-image">
+                {this.renderImages(list.img)}
+                <span className="card-title">Card Title</span>
+                <a
+                  className="btn-floating halfway-fab waves-effect waves-light red"
+                  onClick={() =>
+                    this.handleForm(
+                      list.ListPrice,
+                      list.TaxAnnualAmount,
+                      list.img.length > 0 ? list.img[0] : []
+                    )
+                  }
+                >
+                  <i class="material-icons">add</i>
+                </a>
+              </div>
+              <div className="card-content">
+                <p>List Price {list.ListPrice}</p>
+                <p>Annual Tax Amount {list.TaxAnnualAmount}</p>
+              </div>
+            </div>
+          </div>
+
+
+
+
+
+
+          <div className="col s3">
+            <div className="card">
+              <div className="card-image">
+                {this.renderImages(list.img)}
+                <span className="card-title">Card Title</span>
+                <a
+                  className="btn-floating halfway-fab waves-effect waves-light red"
+                  onClick={() =>
+                    this.handleForm(
+                      list.ListPrice,
+                      list.TaxAnnualAmount,
+                      list.img.length > 0 ? list.img[0] : []
+                    )
+                  }
+                >
+                  <i class="material-icons">add</i>
+                </a>
+              </div>
+              <div className="card-content">
+                <p>List Price {list.ListPrice}</p>
+                <p>Annual Tax Amount {list.TaxAnnualAmount}</p>
+              </div>
+            </div>
+          </div>
+
+
+           */}
       </div>
-    ))
+      // </div>
+    ));
     return listHtml;
   };
 
@@ -157,7 +253,7 @@ class PropertySearch extends Component {
     API.deleteProperty(id)
       .then(console.log("profile deleted"))
       .catch(err => console.log(err));
-      this.handleLocationReload();
+    this.handleLocationReload();
   };
 
   renderSavedProps = () => {
@@ -165,25 +261,28 @@ class PropertySearch extends Component {
       <div>
         <strong>List Price {savedProps.ListPrice}</strong>
         <p>Annual Tax Amount {savedProps.TaxAnnualAmount}</p>
-        <img 
-        image={savedProps.img}
-        key={savedProps.img}
-        src={savedProps.img}
-        alt={""}
-        style={{ height: "100px", width: "100px" }}
-      />
+        <img
+          image={savedProps.img}
+          key={savedProps.img}
+          src={savedProps.img}
+          alt={""}
+          style={{ height: "100px", width: "100px" }}
+        />
         <button
-          onClick={() => this.deleteProperty(
-            savedProps._id
-          )}
+          onClick={() => this.deleteProperty(savedProps._id)}
           className="btn btn-primary"
           style={{ marginTop: "5px" }}
         >
           Delete Property
         </button>
-        <div> <Link to={"/results"}><button type="button">Let's see some results!</button></Link></div>
+        <div>
+          {" "}
+          <Link to={"/results"}>
+            <button type="button">Let's see some results!</button>
+          </Link>
+        </div>
       </div>
-    ))
+    ));
     return propertyHtml;
   };
 
@@ -199,26 +298,36 @@ class PropertySearch extends Component {
   };
 
   render() {
-    console.log("set state",this.state)
+    console.log("set state", this.state);
     return (
       <div>
         <Navbar />
-        <h3>PropertySearch</h3>
-        <div className="row">
+
+        <div className="row propertyPage">
           <div className="col s1"></div>
-          <div className="col s10 skeleton">
+          <div className="col s10 propertyBox">
             <div className="propertySearch">
               <div className="gallery">
-                <div className="images">{this.renderListings()}</div>
+                <div className="row">{this.renderListings()}</div>
                 <h3>Saved Properties</h3>
-                 <div>{this.renderSavedProps()}</div>
-                 <h3>Hi {this.state.name}, for what you want to pay per month we recomend a property priced at "PRICE GOES HERE"</h3> 
-                 <div> <Link to={"/results"}><button type="button">Let's see what that means to you {this.state.name}</button></Link></div>
-                  </div>
+                <div>{this.renderSavedProps()}</div>
+                <h3>
+                  Hi {this.state.name}, for what you want to pay per month we
+                  recomend a property priced at "PRICE GOES HERE"
+                </h3>
+                <div>
+                  {" "}
+                  <Link to={"/results"}>
+                    <button type="button">
+                      Let's see what that means to you {this.state.name}
+                    </button>
+                  </Link>
+                </div>
               </div>
             </div>
           </div>
-          <div className="col s1"></div>
+        </div>
+        <div className="col s1"></div>
         <div className="row">
           <div className="col s12 links">
             <LinkList />
