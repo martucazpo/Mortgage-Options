e1036b658092c437125b6731706d1bd178b8e301
martucazpo <martucazpo@gmail.com>
2019-10-12 11:23:28 -0700

    server nonsense

--- server.js
+++ server.js
@@ -26,7 +26,7 @@ app.use("/api/users", users);
 app.use(routes);
 
 app.get('*', (req, res) => {
-  res.sendFile(path.join(__dirname, './index.html'));
+  res.sendFile(path.join(__dirname,"client","build", './index.html'));
   });
 
 // DB Config
