055d497906af44754331c39230aef917e72b2d85
martucazpo <martucazpo@gmail.com>
2019-10-11 20:42:54 -0700

    server

--- server.js
+++ server.js
@@ -5,6 +5,10 @@ const app = express();
 const passport = require("passport");
 const users = require("./routes/api/users");
 const routes = require("./routes");
+
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static("client/build"));
+}
 // Bodyparser middleware
 app.use(
   bodyParser.urlencoded({
@@ -29,8 +33,7 @@ app.get('*', (req, res) => {
 //const db = require("./config/keys").mongoURI;
 // Connect to MongoDB
 mongoose
-  .connect(
-   // db,
+  .connect(process,env.MONGODB_URI||
    "mongodb://localhost/mortgageOptionsDev",
     { useNewUrlParser: true,
       useUnifiedTopology: true
@@ -39,4 +42,4 @@ mongoose
   .then(() => console.log("MongoDB successfully connected"))
   .catch(err => console.log(err));
 const port = process.env.PORT || 5000; // process.env.port is Heroku's port if you choose to deploy the app there
-app.listen(port, () => console.log(`Server up and running on port ${port} !`));
+app.listen(port, () => console.log(`Server up and running on port ${port} !`))
