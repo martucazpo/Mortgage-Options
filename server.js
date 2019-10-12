bceb64d0a86a8dff0c312b4ac41d487891821d87
martucazpo <martucazpo@gmail.com>
2019-10-12 09:33:26 -0700

    c

--- server.js
+++ server.js
@@ -9,6 +9,9 @@ const routes = require("./routes");
 
 if (process.env.NODE_ENV === "production") {
   app.use(express.static("client/build"));
+  app.get('*', (req, res) => {
+    res.sendFile(path.join(__dirname,"client","build", './index.html'));
+    });
 }
 
 app.use(
